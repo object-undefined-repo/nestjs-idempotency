a58914a940c985aafa1b4e566de3ba3483c4673f
objectt_undefined <objundef@gmail.com>
2025-11-10 22:13:43 +0200

    removed Promise.resolve in async functions

--- src/repositories/idempotency_inmemory_repository.ts
+++ src/repositories/idempotency_inmemory_repository.ts
@@ -1,14 +1,13 @@
 import { IdempotencyRepository} from "@api/index";
 export class IdempotencyInmemoryRepository implements IdempotencyRepository{
-    preSave(idempotencyKey: string): Promise<void> {
+    public async preSave(idempotencyKey: string): Promise<void> {
         throw new Error("Method not implemented.");
     }
-    update(idempotencyKey: string, response: any): Promise<void> {
+    public async update(idempotencyKey: string, response: any): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    public  find(idempotencyKey: string): Promise<{ response: string } | null>{
-     return Promise.resolve({ response: "ok" });
-
+    public async  find(idempotencyKey: string): Promise<{ response: string } | null>{
+     return { response: "ok" };
     }    
 }
