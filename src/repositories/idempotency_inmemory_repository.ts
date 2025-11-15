380819acdd7c728e10d95824b8d544a0fb2df767
objectt_undefined <objundef@gmail.com>
2025-11-15 09:06:24 +0200

    Implemented  in-memory idempotency repository

--- src/repositories/idempotency_inmemory_repository.ts
+++ src/repositories/idempotency_inmemory_repository.ts
@@ -2,14 +2,18 @@ import { IdempotencyRepository} from "@api/index";
 import { Injectable } from "@nestjs/common";
 @Injectable()
 export class IdempotencyInmemoryRepository implements IdempotencyRepository{
+    private data:Record<string,any> = {};
     public async preSave(idempotencyId: string): Promise<void> {
-        throw new Error("Method not implemented.");
+       this.data[idempotencyId] = {};
     }
     public async update(idempotencyId: string, response: any): Promise<void> {
-        throw new Error("Method not implemented.");
+        this.data[idempotencyId] = response;
     }
 
     public async  find(idempotencyId: string): Promise<{ response: string } | null>{
-     return { response: "ok" };
+        if(this.data[idempotencyId])
+            return { response: "ok" };
+        else
+            return null;
     }    
 }
